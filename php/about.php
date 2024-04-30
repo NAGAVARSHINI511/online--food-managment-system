a94fef5ebc01e2e67647880d62e5110920a3ebb2
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2024-04-30 17:45:53 +0530

    Add files via upload

---
+++ php/about.php
@@ -0,0 +1,220 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+<?php include 'components/user_header.php'; ?>
+<!-- header section ends -->
+
+<div class="heading">
+   <h3>about us</h3>
+   <p><a href="home.php">home</a> <span> / about</span></p>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Welcome! We're passionate about delivering delicious, chef-crafted meals to your doorstep. Our mission is to make healthy eating convenient and enjoyable for everyone. We use locally sourced ingredients, prioritize quality, and support our communities. With us, you can expect exceptional service and satisfaction guaranteed. Join us today and experience the convenience of nutritious meals, delivered with care!</p>
+         <a href="menu.html" class="btn">our menu</a>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+<section class="steps">
+
+   <h1 class="title">simple steps</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/step-1.png" alt="">
+         <h3>choose order</h3>
+         <p>Satisfy your cravings according to your mood.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-2.png" alt="">
+         <h3>fast delivery</h3>
+         <p>your order arrives in time!</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-3.png" alt="">
+         <h3>enjoy food</h3>
+         <p>Food may be essential as fuel for your body , But good food is fuel for the soul.</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- steps section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="title">customer's reivews</h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-1.png" alt="">
+            <p>great food! would like to order again.</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>John deo</h3>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-2.png" alt="">
+            <p>Good family time with these delicious food!</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Annestia</h3>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-3.png" alt="">
+            <p>Good services.On time delivery .</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Grey</h3>
+         </div>
+
+         
+
+        
+
+         
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+<?php include 'components/footer.php'; ?>
+<!-- footer section ends -->=
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   grabCursor: true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+      slidesPerView: 1,
+      },
+      700: {
+      slidesPerView: 2,
+      },
+      1024: {
+      slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ php/home.php
@@ -0,0 +1,193 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+include 'components/add_cart.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+
+
+<section class="hero">
+
+   <div class="swiper hero-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>order online</span>
+               <h3>delicious home-made-foods</h3>
+               <a href="menu.html" class="btn">see menus</a>
+            </div>
+            <div class="image">
+               <img src="food img\delicious-indian-food-tray.jpg" alt="">
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>order online</span>
+               <h3>south-indian foods</h3>
+               <a href="menu.html" class="btn">see menus</a>
+            </div>
+            <div class="image">
+               <img src="south\Ela-Sadya.png" alt="">
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>order online</span>
+               <h3>north-indian foods</h3>
+               <a href="menu.html" class="btn">see menus</a>
+            </div>
+            <div class="image">
+               <img src="north\Thali.png" alt="">
+            </div>
+         </div>
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<section class="category">
+
+   <h1 class="title">food category</h1>
+
+   <div class="box-container">
+      <a href="category.php?category=main dish" class="box">
+         <h3>Main dishes</h3>
+      </a>
+
+      <a href="category.php?category=south indian" class="box">
+         <h3>South-indian</h3>
+      </a>
+
+      <a href="category.php?category=north indian" class="box">
+         <h3>North-indian</h3>
+      </a>
+
+   </div>
+
+</section>
+
+
+
+
+<section class="products">
+
+   <h1 class="title">latest dishes</h1>
+
+   <div class="box-container">
+
+      <?php
+         $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
+         $select_products->execute();
+         if($select_products->rowCount() > 0){
+            while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
+      ?>
+      <form action="" method="post" class="box">
+         <input type="hidden" name="pid" value="<?= ₹fetch_products['id']; ?>">
+         <input type="hidden" name="name" value="<?= ₹fetch_products['name']; ?>">
+         <input type="hidden" name="price" value="<?= ₹fetch_products['price']; ?>">
+         <input type="hidden" name="image" value="<?= ₹fetch_products['image']; ?>">
+         <a href="quick_view.php?pid=<?= ₹fetch_products['id']; ?>" class="fas fa-eye"></a>
+         <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
+         <img src="uploaded_img/<?= ₹fetch_products['image']; ?>" alt="">
+         <a href="category.php?category=<?= ₹fetch_products['category']; ?>" class="cat"><?= ₹fetch_products['category']; ?></a>
+         <div class="name"><?= ₹fetch_products['name']; ?></div>
+         <div class="flex">
+            <div class="price"><span>₹</span><?= ₹fetch_products['price']; ?></div>
+            <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+         </div>
+      </form>
+      <?php
+            }
+         }else{
+            echo '<p class="empty">no products added yet!</p>';
+         }
+      ?>
+
+   </div>
+
+   <div class="more-btn">
+      <a href="menu.php" class="btn">veiw all</a>
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".hero-slider", {
+   loop:true,
+   grabCursor: true,
+   effect: "flip",
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+});
+
+</script>
+
+</body>
+</html>

f95cc11087d4515838947a4fba6daa63df57dc7b
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2024-04-30 17:42:03 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:nagavarshini511@gmail.com">nagavarshini511@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">manglore, india -575006</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">9958741651</a>
+      </div>
+
+   </section>
+
+   <div class="credit">GOOD FOOD GOOD MOOD!!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
